bf73cf6273c3d2e4936128ac32c3d605e6acfa17
stanlyh <eduardostanly@gmail.com>
2025-12-30 23:31:20 -0500

    Clases para And Or y Replace

---
+++ ReplaceExpressionVisitor.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace SpecificationPatternDemo;
+
+internal class ReplaceExpressionVisitor : ExpressionVisitor
+{
+    private readonly Expression _oldValue;
+    private readonly Expression _newValue;
+
+    /// <summary>
+    /// Initializes a new instance of the class
+    /// </summary>
+    /// <param name="oldValue">Old expression to be replaced</param>
+    /// <param name="newValue">A new expression that replaces the old one</param>
+    public ReplaceExpressionVisitor(Expression oldValue, Expression newValue)
+    {
+        _oldValue = oldValue;
+        _newValue = newValue;
+    }
+
+    public override Expression Visit(Expression? node)
+        => (node == _oldValue ? _newValue : base.Visit(node))!;
+}
+
